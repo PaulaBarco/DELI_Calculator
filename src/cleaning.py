a6964463320c08f16b42650886d45dafc5e9a14d
PaulaBarco <paulabarco94@hotmail.com>
2024-02-06 13:31:18 +0100

    update with new code

--- src/cleaning.py
+++ src/cleaning.py
@@ -1,6 +1,45 @@
-#first draft
-# continue here
+import pandas as pd
 
-6*2
+#%%
+df1 = pd.read_excel('aaq0216_datas2.xls', sheet_name='Results - Global Totals')
+ 
+print(df1)
+# %%
+#Substracting the data needed
+df2 = df1.iloc[1:56, 0:16]
 
-5*3
+print(df2)
+# %%
+# ** CLEANING DATASET
+# Change the name of some columns
+df3 = (
+    df2.rename(columns={'Unnamed: 0':'Food group', 'Unnamed: 1': 'Food and Waste', 'Impact / kg Food Balance Sheet functional unit (ex. waste)': 'Land Use (m2) Arable',
+                                  'Unnamed: 3': 'Land Use (m2) Fallow',
+                                  'Unnamed: 4': 'Land Use (m2) Perm Past',
+                                  'Unnamed: 5':'GHG (kg CO2eq, IPCC 2013) LUC',
+                                  'Unnamed: 6':'GHG (kg CO2eq, IPCC 2013) Feed',
+                                  'Unnamed: 7':'GHG (kg CO2eq, IPCC 2013) Farm',
+                                  'Unnamed: 8':'GHG (kg CO2eq, IPCC 2013) Processing',
+                                  'Unnamed: 9':'GHG (kg CO2eq, IPCC 2013) Transport',
+                                  'Unnamed: 10':'GHG (kg CO2eq, IPCC 2013) Packging',
+                                  'Unnamed: 11':'GHG (kg CO2eq, IPCC 2013) Retail',
+                                  'Unnamed: 12':'Acid.(kg SO2eq)',
+                                  'Unnamed: 13':'Eutr. (kg PO43-eq)',
+                                  'Unnamed: 14':'Freshwater (L)',
+                                  'Unnamed: 15':'Str-Wt WU (L eq)'})
+)
+
+print(df3)
+#%%
+#Removing the second row
+#df4 = df3.drop(['Product'])
+df4 = df3.iloc[1:,:]
+print(df4)
+#%%
+#Dropping if all values in the row are nan
+df4 = df3.dropna(how='all') 
+
+print (df4)
+#%%
+#Checking some values
+print(df4['Acid.(kg SO2eq)'].loc[df4.index[10]])
